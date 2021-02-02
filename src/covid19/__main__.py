1757bb9b4e1da8176fd53fb4c6a51920a4960016
denis <denis@27tech.ci>
2021-02-02 20:00:33 +0200

    tune

--- src/covid19/__main__.py
+++ src/covid19/__main__.py
@@ -195,7 +195,7 @@ def main():
         trainer = pl.Trainer(
             accelerator="dpp",
             max_epochs=1000,
-            gpus=[1,2] if torch.cuda.is_available() else 0,
+            # gpus=[1,2] if torch.cuda.is_available() else 0,
             weights_summary="top",
             gradient_clip_val=gradient_clip_val,
             limit_train_batches=100.,  # coment in for training, running valiation every 30 batches
