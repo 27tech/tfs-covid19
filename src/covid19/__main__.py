bfe4d19f665f1d7f14b5fa6e0c3b801b09831dd5
denis <denis@27tech.ci>
2021-02-03 07:32:13 +0200

    tune 7

--- src/covid19/__main__.py
+++ src/covid19/__main__.py
@@ -64,8 +64,8 @@ def main():
         static_categoricals=group_ids,
         # static_reals=groups,
         # time_varying_known_categoricals=["special_days", "month"],
-        variable_groups={"day_name": list(calendar.day_name)},  # group of categorical variables can be treated as one variable
-        time_varying_known_categoricals=['day_name'],
+        # variable_groups={"day_name": list(calendar.day_name)},  # group of categorical variables can be treated as one variable
+        # time_varying_known_categoricals=['day_name'],
         # time_varying_known_reals=list(calendar.day_name),
         # time_varying_unknown_categoricals=[],
         time_varying_unknown_reals=[
