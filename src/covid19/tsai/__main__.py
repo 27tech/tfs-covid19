122feb5eaca7158c601a64baae04df639fc2d849
denis <denis@27tech.ci>
2021-02-16 21:37:25 +0200

    all countries

--- src/covid19/tsai/__main__.py
+++ src/covid19/tsai/__main__.py
@@ -11,27 +11,27 @@ if __name__ == "__main__":
         epochs=1000,
         features=[
             ['existing_nx'],
-            ['delta_existing_nx'],
-            ['existing_nx', 'delta_existing_nx'],
-            ['existing_std', 'delta_existing_std'],
-            ['existing_norm', 'delta_existing_norm'],
-            ['existing_rob', 'delta_existing_rob'],
-
-            ['existing_nx_all', 'delta_existing_nx_all'],
-            ['existing_std_all', 'delta_existing_std_all'],
-            ['existing_norm_all', 'delta_existing_norm_all'],
-            ['existing_rob_all', 'delta_existing_rob_all'],
+            # ['delta_existing_nx'],
+            # ['existing_nx', 'delta_existing_nx'],
+            # ['existing_std', 'delta_existing_std'],
+            # ['existing_norm', 'delta_existing_norm'],
+            # ['existing_rob', 'delta_existing_rob'],
+            #
+            # ['existing_nx_all', 'delta_existing_nx_all'],
+            # ['existing_std_all', 'delta_existing_std_all'],
+            # ['existing_norm_all', 'delta_existing_norm_all'],
+            # ['existing_rob_all', 'delta_existing_rob_all'],
 
         ],
         targets=[
             ['delta_existing_nx']
         ],
-        window=[56],
-        horizon=[14],
+        window=[14, 28, 42, 56, 70, 84],
+        horizon=[7, 14, 28, 35],
         batch_size=[256],
         country_filter=[['Ukraine']],
         region_filter=[None],
-        runs=3,
+        runs=1,
     )
     e.run()
 
