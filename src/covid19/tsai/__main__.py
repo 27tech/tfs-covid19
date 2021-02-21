68498182f1ba04b15df6b4bcd8f6062d0cce1cf5
denis <denis@27tech.ci>
2021-02-21 19:23:23 +0200

    open world dataset

--- src/covid19/tsai/__main__.py
+++ src/covid19/tsai/__main__.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         early_stop_patience=1000,
         epochs=1000,
         features=[
-            ['total_cases_per_million_std', 'new_cases_per_million_std'], #0.160758
+            # ['total_cases_per_million_std', 'new_cases_per_million_std'], #0.160758
             # ['total_cases_per_population_std'],
             # ['total_cases_std', 'new_cases_std'] # 0.462105
             # ['new_cases_smoothed_nx'], 0.877701
@@ -29,13 +29,13 @@ if __name__ == "__main__":
             #     'non_sick_per_population',
             #     'people_vaccinated_per_population'
             # ],
-            # [
-            #     'total_cases_per_population',
-            #     'new_cases_per_population',
-            #     'non_sick_per_population',
-            #     'people_vaccinated_per_population',
-            #     'people_fully_vaccinated_per_population'
-            # ],
+            [
+                'total_cases_per_population_nx',
+                'new_cases_per_population_nx',
+                'non_sick_per_population_nx',
+                'people_vaccinated_per_population_nx',
+                'people_fully_vaccinated_per_population_nx'
+            ],
             # ['existing_nx'],
             # ['existing_std', 'confirmed_std', 'delta_existing_std'] + list(calendar.day_name),
             # ['existing_pop', 'confirmed_pop', 'none_sick_pop', 'delta_existing_pop'],
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         country_filter=[
             # ['United States', 'Italy', 'France', 'Germany', 'United Kingdom', 'Australia', 'Canada'],
             # ['Italy'],
-            ['France'],
+            # ['France'],
             # ['United States'],
 
         ],
